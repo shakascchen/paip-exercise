885e6227fb0b88cbea5291b063d640a81800b27a
Shaka Chen <scchen@libgirl.com>
2018-05-15 10:22:35 +0800

    done all m exercises in chap3

--- src/chap3.lisp
+++ src/chap3.lisp
@@ -5,7 +5,8 @@
   (:export #:dotted-expression
            #:print-expression
            #:length-reduce
-           #:find-all))
+           #:find-all
+           #:sentence))
 (in-package :paip-exercise-chap3)
 
 ;;; 3.1
@@ -92,4 +93,78 @@
           (mapcar (lambda (elt) 1)
                   list)))
 
+;;; 3.10
+(describe 'cl:lcm)
+;; COMMON-LISP:LCM
+;;   [symbol]
+
+;; LCM names a compiled function:
+;;   Lambda-list: (&REST INTEGERS)
+;;   Declared type: (FUNCTION (&REST INTEGER)
+;;                   (VALUES UNSIGNED-BYTE &OPTIONAL))
+;;   Derived type: (FUNCTION (&REST T) (VALUES INTEGER &OPTIONAL))
+;;   Documentation:
+;;     Return the least common multiple of one or more integers. LCM of no
+;;       arguments is defined to be 1.
+;;   Known attributes: foldable, flushable, unsafely-flushable, movable
+;;   Source file: SYS:SRC;CODE;NUMBERS.LISP
+
+(describe 'cl:nreconc)
+;; COMMON-LISP:NRECONC
+;;   [symbol]
+
+;; NRECONC names a compiled function:
+;;   Lambda-list: (X Y)
+;;   Declared type: (FUNCTION (LIST T) (VALUES T &OPTIONAL))
+;;   Documentation:
+;;     Return (NCONC (NREVERSE X) Y).
+;;   Known attributes: important-result
+;;   Source file: SYS:SRC;CODE;LIST.LISP
+
+(describe 'cl:nreverse)
+;; COMMON-LISP:NREVERSE
+;;   [symbol]
+
+;; NREVERSE names a compiled function:
+;;   Lambda-list: (SEQUENCE)
+;;   Declared type: (FUNCTION (SEQUENCE) (VALUES SEQUENCE &OPTIONAL))
+;;   Derived type: (FUNCTION (T) (VALUES SEQUENCE &OPTIONAL))
+;;   Documentation:
+;;     Return a sequence of the same elements in reverse order; the argument
+;;        is destroyed.
+;;   Known attributes: important-result
+;;   Source file: SYS:SRC;CODE;SEQ.LISP
+
+(describe 'cl:nconc)
+;; COMMON-LISP:NCONC
+;;   [symbol]
+
+;; NCONC names a compiled function:
+;;   Lambda-list: (&REST LISTS)
+;;   Declared type: (FUNCTION * (VALUES T &OPTIONAL))
+;;   Derived type: (FUNCTION (&REST T) (VALUES T &OPTIONAL))
+;;   Documentation:
+;;     Concatenates the lists given as arguments (by changing them)
+;;   Source file: SYS:SRC;CODE;LIST.LISP
+
+
+;;; 3.11
+(describe 'acons)
+;; COMMON-LISP:ACONS
+;;   [symbol]
+
+;; ACONS names a compiled function:
+;;   Lambda-list: (KEY DATUM ALIST)
+;;   Declared type: (FUNCTION (T T T) (VALUES CONS &OPTIONAL))
+;;   Documentation:
+;;     Construct a new alist by adding the pair (KEY . DATUM) to ALIST.
+;;   Inline proclamation: MAYBE-INLINE (inline expansion available)
+;;   Known attributes: flushable, unsafely-flushable, movable
+;;   Source file: SYS:SRC;CODE;LIST.LISP
+
+;;; 3.12
+(defun sentence (word-list)
+  (format t
+          "~@(~{~a~^ ~}~)."
+          word-list))
 

2e86bd9f57f87e75815b7944f053db185e7015a3
Shaka Chen <scchen@libgirl.com>
2018-06-06 12:44:33 +0800

    reindent 4.1

--- src/chap4.lisp
+++ src/chap4.lisp
@@ -9,12 +9,12 @@
 (in-package :paip)
 (defun dbg-indent (id indent format-string &rest args)
   "Print indented debugging info if (DEBUG ID) has been specified."
-    (format *debug-io*
-            "~:[~;~&~V@T~?~]"
-            (member id *dbg-ids*)
-            (* 2 indent)
-            format-string
-            args))
+  (format *debug-io*
+          "~:[~;~&~V@T~?~]"
+          (member id *dbg-ids*)
+          (* 2 indent)
+          format-string
+          args))
 
 (in-package :paip-exercise-chap4)
 
